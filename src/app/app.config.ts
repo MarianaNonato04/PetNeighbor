ed6ccea3b4b08d44f5a7f6e2329716ad2153dae6
Mariana Nonato <mariananonato013@gmail.com>
2026-06-12 10:09:42 -0300

    ajuste pro githubPages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,1 +1,1 @@
-import { ApplicationConfig } from '@angular/core';import { provideRouter } from '@angular/router';import { routes } from './app.routes';export const appConfig: ApplicationConfig = {  providers: [    provideRouter(routes)  ]};
+import { ApplicationConfig } from '@angular/core';import { provideRouter, withHashLocation } from '@angular/router';import { routes } from './app.routes';export const appConfig: ApplicationConfig = {  providers: [    provideRouter(routes, withHashLocation())  ]};
